d025c1bcb2e3abee1673e34c66e63d7b15dd95f4
Santino0912 <ssantinomunnoz@gmail.com>
2023-08-09 10:19:43 -0300

    Copie el codigo dentro de la pag de Firebase

---
+++ src/app/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environments= {
+    production: false,
+        firebaseConfig: {
+        apiKey: "AIzaSyDXdGx0cHDyPwJUs7uZ4kHUF3uZDesC1rs",
+        authDomain: "prueba-0-a7583.firebaseapp.com",
+        projectId: "prueba-0-a7583",
+        storageBucket: "prueba-0-a7583.appspot.com",
+        messagingSenderId: "925382358121",
+        appId: "1:925382358121:web:ebef332a16e524cae75ef8",
+        measurementId: "G-FNZS75LRE7"
+      }
+}
